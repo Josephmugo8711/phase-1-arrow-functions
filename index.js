adb0bfd664231aa7c6dd8db20d62fac1afd983ba
Joseph <wamiti8711@gmail.com>
2022-11-24 15:37:06 -0500

    arrow function

---
+++ index.js
@@ -0,0 +1,15 @@
+//Anonymous function is a function that
+// does not have any name assosiated with it
+const  divide = function() {
+    return 2000 / 100
+}
+
+divide();
+
+const square = (x) => x*x;
+
+square();
+
+const add = (a,b) => a + b;
+
+add();
